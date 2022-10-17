0f146638af71dcd5b86c783c62437c731d138bf1
RHoganD <richpatric@hotmail.com>
2022-10-17 21:17:17 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+     //The below syntax is more modern, what it's going  to do is go through our buttons array and return  
+       // each element in the array which will be stored in  that variable button on each iteration.   
+    for (let button of buttons){ 
+        button.addEventListener("click", function(){
+    //So let's add an if statement, we're going to  check the attribute of the data type to see what its value is.
+//And if it's submit, we're going  to display an alert that says, "you clicked submit".  
+   if (this.getAttribute("data-type") === "submit") { //  . Inside the code block 'this' refers  
+    alert("You clicked Submit!");                     // to the button that was just clicked so if we click  the addition 
+      }else {                                     //button then 'this' is referring to that specific button
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);   //And then we're using the dollar sign and  curly braces to refer to the game type.                                
+      }  
+
+   
+        });
+    }
+});
+
 function runGame() {
 
 }
